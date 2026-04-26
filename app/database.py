62c694205db6cfceeac89d687417495a69dcf02b
figueroaignacio <ignaciofigueroadev@gmail.com>
2026-04-26 18:01:22 -0300

    fix: database and main

--- app/database.py
+++ app/database.py
@@ -1,15 +1,23 @@
 import os
 
 from dotenv import load_dotenv
+from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "")
 
-engine = create_async_engine(DATABASE_URL, echo=False, connect_args={"ssl": "require"})
+connect_args = {}
+if "sslmode=require" in DATABASE_URL:
+    DATABASE_URL = DATABASE_URL.replace("?sslmode=require", "").replace(
+        "&sslmode=require", ""
+    )
+    connect_args["ssl"] = "require"
+
+engine = create_async_engine(DATABASE_URL, echo=False, connect_args=connect_args)
 
 AsyncSessionLocal = sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
@@ -18,6 +26,7 @@ AsyncSessionLocal = sessionmaker(
 
 async def init_db():
     async with engine.begin() as conn:
+        await conn.execute(text("CREATE EXTENSION IF NOT EXISTS vector"))
         await conn.run_sync(SQLModel.metadata.create_all)
 
 
